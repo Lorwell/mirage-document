b8a341cf015684d5f2365e3ca01af39d654b151a
思追ï¼(shaco) <1679924785@qq.com>
2023-02-27 18:31:58 +0800

    feat: 完善文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'mirage',
+    description: 'mirage 文档',
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: [
+            ['/', "主页"],
+            ['/guide/', '指南'],
+        ]
+    },
+    markdown: {
+        lineNumbers: true
+    }
+}
